29c3d5d6b478eabd7c7968516ff50391cc6ee5aa
Thoffy <126025184+Thoffyy@users.noreply.github.com>
2025-07-07 17:11:35 -0400

    Add files via upload

---
+++ simplewhile.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int num, square;
+
+
+int main(){
+
+printf("digite um numero int");
+scanf("%d", &num) ;
+
+while (num !=0)
+{
+    square = num * num;
+
+    printf("quadrado e: %d \n" , square );
+
+
+    printf("digite um numero int");
+    scanf ("%d", &num);
+
+
+    }
+
+return 0;
+
+}
